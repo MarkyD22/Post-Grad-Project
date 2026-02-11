6f625b52818a275c8227c55eb3f3a9345f7b7deb
MarkyD22 <154269554+MarkyD22@users.noreply.github.com>
2026-02-11 14:33:00 +0000

    Changes to Allowed hosts and security updates
    added project to allowed hosts. Turned off debugging

--- S00044234_Maint_Calib_Db/settings.py
+++ S00044234_Maint_Calib_Db/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_ya&$bezjt8tj+!_22*h=^72y^ri4j=5^@&=-th2^+*w(20r60'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['post-grad-project.onrender.com']
 
 
 # Application definition
